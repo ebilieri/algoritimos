c960ee62406ce8ee4faab2b244843e8937dc2372
Emerson Bilieri <ebilieri@hotmail.com>
2020-08-31 09:46:51 -0300

    Algoritimo leftRotate

---
+++ AlgoritmosHackerC#/LeftRotarionArray/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace LeftRotarionArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+            int d = 2;
+           
+            int[] ret = LeftRotateNoPerformatic(arr, d);
+            PrintArray(ret);
+
+            Console.WriteLine("Case 2");
+            arr = new int[] { 1, 2, 3, 4, 5 };
+            d = 4;
+           
+            ret = LeftRotateNoPerformatic(arr, d);
+            PrintArray(ret);
+
+            Console.WriteLine("Case 3");
+            arr = new int[] { 1, 2, 3, 4, 5 };
+            d = 4;
+            ret = LetfRotate(arr, d);
+            PrintArray(ret);
+        }
+
+        private static void PrintArray(int[] ret)
+        {
+            foreach (var item in ret)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static int[] LeftRotateNoPerformatic(int[] arr, int d)
+        {
+            for (int i = 0; i < d; i++)
+            {
+                int j, temp = arr[0];
+                for (j = 0; j < arr.Length - 1; j++)
+                {
+                    arr[j] = arr[j + 1];
+                }
+
+                arr[j] = temp;
+            }
+
+            return arr;
+        }
+
+        static int[] LetfRotate(int[] arr, int d)
+        {            
+            int[] newArr = new int[arr.Length];
+            
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int newlocation = (i + (arr.Length - d)) % arr.Length;
+                newArr[newlocation] = arr[i];
+            }
+
+            return newArr;
+        }
+
+
+    }
+}
---
+++ AlgoritmosHackerC#/MakeAnagram/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MakeAnagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "cde";
+            string b = "abc";
+
+            int ret = MakeAnagram(a, b);
+            Console.WriteLine(ret);
+
+            a = "fcrxzwscanmligyxyvym";
+            b = "jxwtrhvujlmrpdoqbisbwhmgpmeoke";
+            ret = MakeAnagram(a, b);
+            Console.WriteLine(ret);
+        }
+
+
+        static int MakeAnagram(string a, string b)
+        {
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            int length = 0;
+
+            foreach (var c in a)
+            {
+                if (dict.ContainsKey(c))
+                    dict[c] = dict[c] + 1;
+                else
+                    dict.Add(c, 1);
+            }
+
+            foreach (var c in b)
+            {
+                if (dict.ContainsKey(c) && dict[c] != 0)
+                {
+                    dict[c] = dict[c] - 1;
+                    length += 2;
+                }
+            }
+
+            return a.Length + b.Length - length;
+        }
+
+    }
+}
