7ebc7bf8f8b9571f2c9c4f0a859727dcc431bb21
Emerson Bilieri <ebilieri@hotmail.com>
2020-09-03 13:59:20 -0300

    print data

--- AlgoritmosHackerC#/BigSortingArray/Program.cs
+++ AlgoritmosHackerC#/BigSortingArray/Program.cs
@@ -8,28 +8,24 @@ namespace BigSortingArray
         static void Main(string[] args)
         {
             string[] arr = new string[] { "1", "2", "100", "12303479849857341718340192371", "3084193741082937", "3084193741082938", "111", "200" };
-
-            var ret = BigSorting(arr);
-
-            foreach (var item in ret)
-            {
-                Console.WriteLine(item);
-            }
+            PrintData(arr);
 
             Console.WriteLine("Case 2");
             arr = new string[] { "31415926535897932384626433832795", "1", "3", "10", "3", "5" };
+            PrintData(arr);
 
-            ret = BigSorting(arr);
+            Console.WriteLine("Hello World!");
+        }
 
+        private static void PrintData(string[] arr)
+        {
+
+            var ret = BigSorting(arr);
             foreach (var item in ret)
             {
                 Console.WriteLine(item);
             }
-            Console.WriteLine("Hello World!");
         }
-
-
-
 
         static string[] BigSorting(string[] unsorted)
         {
