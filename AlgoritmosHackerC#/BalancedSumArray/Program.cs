9e4d74e60daeec0632ebb75e945905b9cba3f786
Emerson Bilieri <ebilieri@hotmail.com>
2020-09-03 22:10:52 -0300

    BalancedSumArray

---
+++ AlgoritmosHackerC#/BalancedSumArray/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BalancedSumArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> arr = new List<int> { 1, 2, 3, 3 };
+            int r = BalancedSum(arr);
+            Console.WriteLine(r);
+
+            arr = new List<int> { 1, 2, 1 };
+            r = BalancedSum(arr);
+            Console.WriteLine(r);
+
+            arr = new List<int> { 1, 2, 1, 3, 4, 3, 6, 7, 8, 9, 9, 9 };
+            r = BalancedSum(arr);
+            Console.WriteLine(r);
+        }
+
+        public static int BalancedSum(List<int> arr)
+        {
+            int size = arr.Count;
+            int sumRight = 0;
+            int sumLeft = arr[0];
+
+            // sum right items array
+            for (int i = 2; i < size; i++)
+            {
+                sumRight += arr[i];
+            }
+
+            if (sumLeft == sumRight)
+            {
+                return 1;
+            }
+            else
+            {
+                for (int i = 2; i < size - 1; i++)
+                {
+                    sumLeft += arr[i - 1];
+                    sumRight -= arr[i];
+
+                    if (sumLeft == sumRight)
+                        return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
