0a712cad48c726ba1a94867ae015d36aeb016e7e
Emerson Bilieri <ebilieri@hotmail.com>
2020-09-03 22:05:13 -0300

    SolveMeFirst, SimpleArraySum

---
+++ AlgoritmosHackerC#/BasicAlgorithms/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BasicAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var solveM = SolveMeFirst(2, 3);
+            Console.WriteLine(solveM);
+
+            Console.WriteLine("Simple sum array");
+            int[] inputArray = new int[] { 1, 2, 3, 4, 10, 11 };
+            var sumArray = SimpleArraySum(inputArray);
+            Console.WriteLine(sumArray);
+        }
+
+
+        static int SolveMeFirst(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int SimpleArraySum(int[] ar)
+        {
+            int sum = 0;
+            foreach (var item in ar)
+            {
+                sum += item;
+            }
+
+            return sum;
+        }
+    }
+}
